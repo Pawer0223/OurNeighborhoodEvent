46b925142584f4950fc535c90618581ea2cbaab2
NAME <gbgo77@naver.com>
2020-05-01 18:43:43 +0900

    작은수정.. transaction관리 context-datasource.xml 수정.. 데이터 전달할 때 model클래스 테스트 한번해봄..

--- src/main/java/woodong2/controller/test/TestController.java
+++ src/main/java/woodong2/controller/test/TestController.java
@@ -1,8 +1,12 @@
 package woodong2.controller.test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -21,4 +25,18 @@ public class TestController {
 		return "/test/apiSampleJSON";
 	}
 	
+	@RequestMapping(value = "/modelTest.do")
+	public String modelTest(Model model) throws Exception {
+		
+		System.out.println(" 모델 테스트 . ");
+		
+		Map<String,Object> isModel = new HashMap<>();
+		
+		isModel.put("Test2", "강태산");
+		
+		model.addAttribute("Test","김밥집");
+		
+		
+		return "/test/modelTest";
+	}
 }
---
+++ src/main/webapp/WEB-INF/jsp/test/modelTest.jsp
@@ -0,0 +1,27 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ page import="org.json.*"%>
+
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions"%>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta charset="UTF-8">
+<title>Model Test</title>
+
+<link rel="stylesheet"
+	href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
+<body>
+<h1> Model Test</h1>
+Test: ${Test}
+Test2: ${Test2}
+<br>
+
+
+
+</body>
+</html>
